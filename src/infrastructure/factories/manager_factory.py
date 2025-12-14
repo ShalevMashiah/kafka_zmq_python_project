4983d3daff0fef52b0d2b2da69ee821236bb6aed
Shalev Mashiah <shalev1305@icloud.com>
2025-12-14 09:05:25 +0200

    fix configuration path

--- src/infrastructure/factories/manager_factory.py
+++ src/infrastructure/factories/manager_factory.py
@@ -1,3 +1,4 @@
+import os
 from infrastructure.factories.infrastructure_factory import InfrastructureFactory
 from globals.consts.const_strings import ConstStrings
 from infrastructure.interfaces.iexample_manager import IExampleManager
@@ -7,6 +8,13 @@ from infrastructure.interfaces.ilogger_manager import ILoggerManager
 
 
 class ManagerFactory:
+
+    # def _get_config_path() -> str:
+    #     factories_dir = os.path.dirname(os.path.abspath(__file__))
+    #     infra_root = os.path.dirname(factories_dir)
+    #     config_path = os.path.join(infra_root, "config", "configuration.xml")
+    #     return config_path
+    
     @staticmethod
     def create_example_manager() -> IExampleManager:
         config_manager = InfrastructureFactory.create_config_manager(
